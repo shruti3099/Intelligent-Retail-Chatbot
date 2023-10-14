e2ac16712702185f5d88e063cb00a4d057fc09d6
Shruti Dash <shrutidash660@gmail.com>
2023-10-14 19:10:37 -0400

    Update prompt.py

--- v2/prompt.py
+++ v2/prompt.py
@@ -1,66 +1,103 @@
 import streamlit as st
-import mysql.connector
 
-# MySQL connection configuration
-mysql_config = {
-    'user': 'root',
-    'password': 'Tina3099*',
-    'host': '127.0.0.1',
-    'database': 'RetailData'  # Replace with your actual schema/database name
+QUALIFIED_TABLE_NAMES = [
+    "RetailData.customers",
+    "RetailData.categories"]
+
+TABLE_DESCRIPTIONS = {
+    "RetailData.customers": """
+        This table has customer data for people who ordered from Big Supply
+        """,
+    "RetailData.categories": """
+        This table has information about different categories at Big Supply
+        """
 }
+# This query is optional if running Frosty on your own table, especially a wide table.
+# Since this is a deep table, it's useful to tell Frosty what variables are available.
+# Similarly, if you have a table with semi-structured data (like JSON), it could be used to provide hints on available keys.
+# If altering, you may also need to modify the formatting logic in get_table_context() below.
+#METADATA_QUERY = "SELECT VARIABLE_NAME, DEFINITION FROM FROSTY_SAMPLE.CYBERSYN_FINANCIAL.FINANCIAL_ENTITY_ATTRIBUTES_LIMITED;"
 
-# List of tables with their descriptions
-TABLES = [
-    {
-        "name": "customers",
-        "description": "customer data"
-    },
-    {
-        "name": "categories",
-        "description": "category data"
-    },
-    # Add more tables as needed in a similar format
-]
+GEN_SQL = """
+You will be acting as an AI Snowflake SQL Expert named Chatbot.
+Your goal is to give correct, executable sql query to users.
+You will be replying to users who will be confused if you don't respond in the character of Chatbot.
+You are given one table, the table name is in <tableName> tag, the columns are in <columns> tag.
+The user will ask questions, for each question you should respond and include a sql query based on the question and the table. 
 
-# Function to get column names for a given table
-def get_columns_for_table(table_name, connection):
-    cursor = connection.cursor()
-    cursor.execute(f"SHOW COLUMNS FROM {table_name}")
-    columns = [column[0] for column in cursor.fetchall()]
-    cursor.close()
-    return columns
+{context}
 
-# Modify get_table_context to handle multiple tables and fetch column names
+Here are 6 critical rules for the interaction you must abide:
+<rules>
+1. You MUST MUST wrap the generated sql code within ``` sql code markdown in this format e.g
+```sql
+(select 1) union (select 2)
+```
+2. If I don't tell you to find a limited set of results in the sql query or question, you MUST limit the number of responses to 10.
+3. Text / string where clauses must be fuzzy match e.g ilike %keyword%
+4. Make sure to generate a single snowflake sql code, not multiple. 
+5. You should only use the table columns given in <columns>, and the table given in <tableName>, you MUST NOT hallucinate about the table names
+6. DO NOT put numerical at the very front of sql variable.
+</rules>
+
+Don't forget to use "ilike %keyword%" for fuzzy match queries (especially for variable_name column)
+and wrap the generated sql code with ``` sql code markdown in this format e.g:
+```sql
+(select 1) union (select 2)
+```
+
+For each question from the user, make sure to include a query in your response.
+
+Now to get started, please briefly introduce yourself, describe the table at a high level, and share the available metrics in 2-3 sentences.
+Then provide 3 example questions using bullet points.
+"""
+
 @st.cache_data(show_spinner=False)
-def get_table_context(tables, connection):
-    context = ""
-    for table_info in tables:
-        table_name = table_info['name']
-        table_description = table_info['description']
-        # Fetch column information for each table
-        columns = get_columns_for_table(table_name, connection)
-        columns_info = "\n".join([f"- **{col}**: Data Type" for col in columns])
-        context += f"""
-        \n\nHere is the table name <tableName> {table_name} </tableName>
-        \n<tableDescription>{table_description}</tableDescription>
-        \n\nHere are the columns of the {table_name}
-        \n<columns>\n\n{columns_info}\n\n</columns>"""
-    return context
+def get_table_context(table_name: str, table_description: str, metadata_query: str = None):
+    table = table_name.split(".")
+    conn = st.experimental_connection("RetailData", type='sql')
+    columns = conn.query(f"""
+        SELECT COLUMN_NAME, DATA_TYPE FROM {table[0].upper()}.INFORMATION_SCHEMA.COLUMNS
+        WHERE TABLE_SCHEMA = '{table[1].upper()}' AND TABLE_NAME = '{table[2].upper()}'
+        """,
+    )
+    columns = "\n".join(
+        [
+            f"- **{columns['COLUMN_NAME'][i]}**: {columns['DATA_TYPE'][i]}"
+            for i in range(len(columns["COLUMN_NAME"]))
+        ]
+    )
+    context = f"""
+Here is the table name <tableName> {'.'.join(table)} </tableName>
 
-# Modify get_system_prompt to handle multiple tables
-def get_system_prompt(tables, connection):
-    table_context = get_table_context(tables, connection)
+<tableDescription>{table_description}</tableDescription>
+
+Here are the columns of the {'.'.join(table)}
+
+<columns>\n\n{columns}\n\n</columns>
+    """
+    # if metadata_query:
+    #     metadata = conn.query(metadata_query)
+    #     metadata = "\n".join(
+    #         [
+    #             f"- **{metadata['VARIABLE_NAME'][i]}**: {metadata['DEFINITION'][i]}"
+    #             for i in range(len(metadata["VARIABLE_NAME"]))
+    #         ]
+    #     )
+    #     context = context + f"\n\nAvailable variables by VARIABLE_NAME:\n\n{metadata}"
+    # return context
+
+def get_system_prompt():
+table_context = ""
+for table_name in QUALIFIED_TABLE_NAMES:
+table_context += get_table_context(
+table_name=table_name,
+table_description=TABLE_DESCRIPTIONS.get(table_name, ""),
+#metadata_query=METADATA_QUERIES.get(table_name, None)
+)
     return GEN_SQL.format(context=table_context)
 
 # do `streamlit run prompts.py` to view the initial system prompt in a Streamlit app
 if __name__ == "__main__":
-    st.header("System prompt for Frosty")
-
-    # Establish MySQL connection
-    try:
-        connection = mysql.connector.connect(**mysql_config)
-        st.markdown("MySQL connection successful!")
-        st.markdown(get_system_prompt(TABLES, connection))
-        connection.close()
-    except mysql.connector.Error as err:
-        st.error(f"Error: Unable to connect to MySQL - {err}")
+    st.header("System prompt for Chatbot")
+    st.markdown(get_system_prompt())
